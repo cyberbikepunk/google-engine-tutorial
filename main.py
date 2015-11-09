b95f47dac1d0d3a4fe31b47d10529cb8a0842b71
cyberbikepunk <loic@cyberpunk.bike>
2015-11-09 22:52:29 +0100

    Writing my first form

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
+import cgi
 from google.appengine.api import users
+from webapp2 import RequestHandler, WSGIApplication
 
-from webapp2 import WSGIApplication, RequestHandler
+
+MAIN_PAGE_HTML = """\
+<html>
+  <body>
+    <form action="/sign" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Sign Guestbook"></div>
+    </form>
+  </body>
+</html>
+"""
 
 
 class MainPage(RequestHandler):
-
     def get(self):
-        # Checks for active Google account session
-        user = users.get_current_user()
-
-        if user:
-            self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
-            self.response.write('Hello, ' + user.nickname())
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+        self.response.write(MAIN_PAGE_HTML)
 
 
-app = WSGIApplication([('/', MainPage), ], debug=True)
+class Guestbook(RequestHandler):
+    def post(self):
+        self.response.write('<html><body>You wrote:<pre>')
+        self.response.write(cgi.escape(self.request.get('content')))
+        self.response.write('</pre></body></html>')
 
+
+app = WSGIApplication([('/', MainPage), ('/sign', Guestbook), ], debug=True)
